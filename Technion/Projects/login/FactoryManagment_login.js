a762017250ca6a1d153567d87f394cef2f7c797c
yizharf9 <87658085+yizharf9@users.noreply.github.com>
2022-01-21 18:48:32 +0200

    login renforced with password and messages

--- Technion/Projects/login/FactoryManagment_login.js
+++ Technion/Projects/login/FactoryManagment_login.js
@@ -2,9 +2,7 @@ let Username = document.getElementById('Username')
 let Password = document.getElementById('Password')
 
 function test(){
-    console.log(Username.value)
-    console.log(Password.value)
-
+    let 
 }
 
 async function getData() {
@@ -17,7 +15,7 @@ async function getData() {
         if (user.userName == Username |user.password == Password) {
             auth = true
             console.log(user)
-        }
+        }else{alert()}
     })
 }
 
@@ -36,11 +34,24 @@ async function login() {
     let resp = await fetch('https://localhost:44399/api/login',Params)
 
     let data = await resp.json()
-    if (data) {
-        window.open('./FactoryManagment_home_page.html', '_blank',)
+    console.log(data.auth)
+    if (data.auth) {
+        //window.open('./FactoryManagment_home_page.html', '_blank',)
+        let temp = document.getElementById('loginInp')
+        temp.innerHTML = ""
+        let authBox = document.createElement('a')
+        authBox.href = "./FactoryManagment_home_page.html?username="
+        authBox.innerText = "You are being redirected to your'e home page"
+        
+        let br = document.createElement('br')
+        temp.appendChild(br)
+        temp.appendChild(br)
+        temp.appendChild(br)
+
+        temp.appendChild(authBox)
     }else{
-        alert('invalid password or username!')
+        alert(data.username)
     }
     return await data
 }
-
+console.log('hello world')
