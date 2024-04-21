475447d17ffb16b1fb89c47abf872f951fd909e3
yizhar <yizharf9@gmail.com>
2024-04-21 19:10:29 +0300

    fixed last commit, get_routes : fully working!

--- python/ex9.py
+++ python/ex9.py
@@ -3,7 +3,7 @@ sample_path = "../rust/advent_of_code2015/src/sample.txt"
 
 roads = {}
 locations = set()
-with open(excercice_path) as f:
+with open(sample_path) as f:
     f = f.readlines()
     for line in f:
         terms = line.split()
@@ -20,15 +20,14 @@ print(all_locations)
 
 def get_routes(locations:list[str],current_route:list[str]=[],routes:list[list[str]]=[]):
     # print(f"{current_route}\n")
+    
     if len(locations) == 0:        
         routes.append(current_route)
-        return routes
-    
+        
     for i in range(len(locations)):
         possible_locations = locations[:i]+locations[i+1:]
         next_route = current_route + [locations[i]]
-        routes += get_routes(possible_locations,next_route) #routes(+1)
-    return routes
+        get_routes(possible_locations,next_route,routes) 
 
 global _routes
 _routes = []
@@ -42,13 +41,12 @@ def get_routes_global(locations:list[str],current_route:list[str]=[]):
         possible_locations = locations[:i]+locations[i+1:]
         next_route = current_route + [locations[i]]
         get_routes_global(possible_locations,next_route)
+result = []
+get_routes(all_locations,[],result)
 
-
-
-result = get_routes(all_locations)
-for route in _routes:
+for route in result:
     print(route)
-    
+
 def get_total_distance(route:list[str],roads:list[(str,str,int)]):
     total_distance = 0
     for i in range(len(route)-1):
