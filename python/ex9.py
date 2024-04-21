de514844f6e5b44a8df0ac1b9db007f53b352fec
yizhar <yizharf9@gmail.com>
2024-04-21 19:03:47 +0300

    get_routes : fully working!

--- python/ex9.py
+++ python/ex9.py
@@ -18,29 +18,22 @@ all_locations = [loc for loc in locations]
 
 print(all_locations)
 
-# def get_routes(locations:list[str],current_route:list[str]=[],routes:list[list[str]]=[]):
-#     print(f"{current_route}\n")
-#     if len(locations) == 0:        
-#         return []
+def get_routes(locations:list[str],current_route:list[str]=[],routes:list[list[str]]=[]):
+    # print(f"{current_route}\n")
+    if len(locations) == 0:        
+        routes.append(current_route)
+        return routes
     
-#     for i in range(len(locations)):
-#         possible_locations = locations[:i]+locations[i+1:]
-#         next_route = current_route + [locations[i]]
-        
-#         possible_routes = get_routes(possible_locations,next_route,routes)
-#         if len(possible_routes) == 0:
-#             routes.append(possible_routes + next_route)
-#         else:
-#             for route in possible_routes:
-#                 route = current_route + route
-#                 routes.append(route)
-                
-#     return routes
+    for i in range(len(locations)):
+        possible_locations = locations[:i]+locations[i+1:]
+        next_route = current_route + [locations[i]]
+        routes += get_routes(possible_locations,next_route) #routes(+1)
+    return routes
 
 global _routes
 _routes = []
 
-def get_routes(locations:list[str],current_route:list[str]=[]):
+def get_routes_global(locations:list[str],current_route:list[str]=[]):
     # print(f"{current_route}\n")
     if len(locations) == 0:        
         return _routes.append(current_route)
@@ -48,11 +41,11 @@ def get_routes(locations:list[str],current_route:list[str]=[]):
     for i in range(len(locations)):
         possible_locations = locations[:i]+locations[i+1:]
         next_route = current_route + [locations[i]]
-        get_routes(possible_locations,next_route)
+        get_routes_global(possible_locations,next_route)
 
 
 
-get_routes(all_locations)
+result = get_routes(all_locations)
 for route in _routes:
     print(route)
     
