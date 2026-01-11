b7c83ebcff24b447bf248450894c9eb836108fd6
yizharf9 <yizharf9@gmail.com>
2026-01-11 23:12:31 +0200

    algo_design ex2 : preprocessing is robust for now

--- bgu/algo_design/ex2/ex2.py
+++ bgu/algo_design/ex2/ex2.py
@@ -1,35 +1,58 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import sys
 
+import networkx as nx #! remove before submission , will be graded 0!!!
 
-def initiate_sample_data_file(path = "./data.txt",N = 10):
-    data_mat = np.random.rand(N,N) <= 0.5
-    data_mat = np.array(data_mat,dtype=int)
-    print(data_mat)
-    
-    with open(path,"w") as f :
-        lines = []
-        for i in range(N) :
-            added_line = ""
-            for j in range(N):
-                if i != j :
-                    added_line += str(data_mat[i,j]) + " "
-                else :
-                    added_line += str(0) + " "
-            lines.append(added_line + "\n")
-        f.writelines(lines)
+def initiate_sample_data_file(path = "./data.txt",N = 10 ,p = 0.25,test_example = True):
+    if not test_example :
+        data_mat = np.random.rand(N,N) <= p
+        data_mat = np.array(data_mat,dtype=int)
+        print(data_mat)
+        
+        T = np.random.randint(1,N,N)
+        G = np.random.randint(1,N,N)
+        with open(path,"w") as f :
+            lines = []
+            for i in range(N) :
+                added_line = ""
+                for j in range(N):
+                    if i != j :
+                        added_line += str(data_mat[i,j]) + " "
+                    else :
+                        added_line += str(T[i]) + "," + str(G[i]) + " "
+                lines.append(added_line + "\n")
+            f.writelines(lines)
+    else : 
+        with open(path,"w") as f :
+            mat_string = "2,5 1 0 0\n1 4,1 1 0\n0 0 3,2 1\n0 0 0 1,4"
+            f.write(mat_string)
 
-def read_data_file(path = "./data.txt",p_flag = True):
+def read_data_file(path = "./data.txt",p_flag = True): #? make sure this answers the actual format of the input
     lines = []
+    T = []
+    G = []
     with open(path,"r") as f :
-        for line in f.readlines():
-            line = line[:-2]
+        for i,line in enumerate(f.readlines()):
+            line = line.strip()
             line = line.split(" ")
+            ti,gi = line[i].split(',')
+            line[i] = "0"
+            if p_flag : 
+                print(line)
+                print(i,ti,gi)
+            
+            T.append(int(ti))
+            G.append(int(gi))
             lines.append([int(s) for s in line])
-            if p_flag : print(line)
+            
+
     data = np.array(lines)
-    if p_flag : print(data)
-    return data
+    if p_flag : 
+        print(f"data : \n{data}")
+        print(f"T : {T}")
+        print(f"G : {G}")
+    return data,T,G
 
 def get_path(adj_mat:np.ndarray,source:int,target:int,path = [])->list[int]:
     if source == target :
@@ -45,7 +68,7 @@ def get_path(adj_mat:np.ndarray,source:int,target:int,path = [])->list[int]:
     return 
 
 if __name__ == "__main__":
-    # initiate_sample_data_file()
+    initiate_sample_data_file(test_example=True)
     if len(sys.argv) != 2 :
         print("invalid arg list for execution!")
         exit()
@@ -53,9 +76,19 @@ if __name__ == "__main__":
     file_path = sys.argv[1]
     print(f"data path = {file_path}")
     
-    data = read_data_file(path=file_path,p_flag=False)
-    print(data)
+    data,T,G = read_data_file(path=file_path,p_flag=False)
+    N = data.shape[0]
     
-    get_path(data,0,4)
+    print(f"\ndata : \n{data}\n")
+    print(f"T : {T}")
+    print(f"G : {G}")
+    print(f"N : {N}\n\n")
+    
+    
+    # G = nx.DiGraph(data)
+    # nx.draw(G,with_labels=True,pos=nx.circular_layout(G))
+    # plt.show()
+    
+    # get_path(data,0,4)
     
     
--- bgu/gen_models/orthogonal_diff/setup/package_setup_script_windows.ps1
+++
@@ -1,7 +0,0 @@
-echo off ; 
-echo Creating virtual environment... ;
-python -m venv .venv ;
-echo Activating environment and installing libraries... ; 
-.venv\Scripts\activate ;
-pip install -r requirements.txt ;
-echo Setup complete! ;
