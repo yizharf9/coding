8086e6b137839567635faaefe9cc4871ca5f8fe2
yizharf9 <yizharf9@gmail.com>
2026-01-12 02:37:59 +0200

    algo_design ex2 : given assignment example validated, edge cases needed reassurance before submission!

--- bgu/algo_design/ex2/ex2.py
+++ bgu/algo_design/ex2/ex2.py
@@ -4,6 +4,76 @@ import sys
 
 import networkx as nx #! remove before submission , will be graded 0!!!
 
+class TarjanSCC() :
+    def __init__(self,adj_mat:np.ndarray,T:list[int],G:list[int]):
+        self.adj_mat = adj_mat
+        N = adj_mat.shape[0]
+        
+        self.adj_list = []
+        for node in range(N):
+            node_neighbors = []
+            for neighbor in range(N):
+                if adj_mat[node,neighbor] == 1:
+                    node_neighbors.append(neighbor)
+            self.adj_list.append(node_neighbors)
+        
+        self.real_T = T
+        self.G = G
+        
+        self.IDs = [-1]*N
+        self.Low = [0]*N
+        
+        self.node_stack = [] # all nodes initiated to not discovered in initialization
+        self.nodes_in_stack = [False]*N # all nodes not in stack in initialization
+        self.timer = 0
+        
+        self.SCC_count = -1
+    
+    def time_inc(self):
+        self.timer += 1
+    
+    def DFS(self,at:int,p_flag = True):
+        
+        self.node_stack.append(at)
+        self.nodes_in_stack[at] = True
+        
+        self.IDs[at] = self.timer
+        self.Low[at] = self.timer
+        
+        self.time_inc()
+        N = self.adj_mat.shape[0]
+        
+        if p_flag : 
+            print(f"\ntime:{self.timer}")
+            print(f"at : {at}")
+            
+        for to in self.adj_list[at]:
+            if p_flag : 
+                print(f"to : {to}",end=" ") 
+                print()
+            if self.IDs[to] == -1 : 
+                self.DFS(to,p_flag)
+            if self.nodes_in_stack[to] :
+                self.Low[at] = min(self.Low[at],self.Low[to])
+        
+        
+        if self.IDs[at] == self.Low[at] :
+            while len(self.node_stack) > 0 :
+                node = self.node_stack.pop()
+                self.nodes_in_stack[node] = False
+                self.Low[node] = self.IDs[at]
+                if node == self.IDs[at] :
+                    break
+        self.SCC_count +=1
+    
+    def count_SCC(self,P_flag = True):
+        N = len(self.adj_list)
+        for i in range(N):
+            if self.IDs[i] == -1 :
+                self.DFS(i,p_flag=P_flag)
+        return self.Low
+
+
 def initiate_sample_data_file(path = "./data.txt",N = 10 ,p = 0.25,test_example = True):
     if not test_example :
         data_mat = np.random.rand(N,N) <= p
@@ -33,6 +103,8 @@ def read_data_file(path = "./data.txt",p_flag = True): #? make sure this answers
     T = []
     G = []
     with open(path,"r") as f :
+        if len(f.read()) == 0 : # empty graph
+            return np.array([[]]),T,G
         for i,line in enumerate(f.readlines()):
             line = line.strip()
             line = line.split(" ")
@@ -54,21 +126,9 @@ def read_data_file(path = "./data.txt",p_flag = True): #? make sure this answers
         print(f"G : {G}")
     return data,T,G
 
-def get_path(adj_mat:np.ndarray,source:int,target:int,path = [])->list[int]:
-    if source == target :
-        path.append(target)
-        return
-    _neighbors = adj_mat[source,:]
-    for i,neighbor in enumerate(_neighbors):
-        if neighbor == 1 :
-            print(i)
-            path.append(i)
-            adj_mat[source,i] = 0 
-            get_path(adj_mat,i,target,path)
-    return 
 
 if __name__ == "__main__":
-    initiate_sample_data_file(test_example=True)
+    # initiate_sample_data_file(test_example=True)
     if len(sys.argv) != 2 :
         print("invalid arg list for execution!")
         exit()
@@ -79,15 +139,24 @@ if __name__ == "__main__":
     data,T,G = read_data_file(path=file_path,p_flag=False)
     N = data.shape[0]
     
-    print(f"\ndata : \n{data}\n")
-    print(f"T : {T}")
+    tar = TarjanSCC(data,T,G)
+    
+    
+    print(f"\nadj_mat : \n{data}\n")
+    print(f"\nadj_list : ")
+    for l in tar.adj_list:
+        print(l)
+    print(f"\nT : {T}")
     print(f"G : {G}")
     print(f"N : {N}\n\n")
     
+    lows = tar.count_SCC(P_flag=False)
+    print(f"low link values : {lows}")
     
-    # G = nx.DiGraph(data)
-    # nx.draw(G,with_labels=True,pos=nx.circular_layout(G))
-    # plt.show()
+    #! remove before submission , will be graded 0!!!
+    G = nx.DiGraph(data)
+    nx.draw(G,with_labels=True,pos=nx.circular_layout(G))
+    plt.show()
     
     # get_path(data,0,4)
     
